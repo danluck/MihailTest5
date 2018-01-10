a77c651e835e88e92568e2c8651af6df7133c44f
YoPc <danluck@mail.ru>
2018-01-10 21:43:21 +0300

    Add check of device opportunities

--- app/src/main/java/com/example/yo/mihailtest5/MainActivity.java
+++ app/src/main/java/com/example/yo/mihailtest5/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yo.mihailtest5;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckDeviceOpportunities();
     }
+
+
+    private void CheckDeviceOpportunities() {
+        if (mBluetoothAdapter == null) {
+            Log.d(TAG, "Bluetooth is not supported on this device");
+            Toast.makeText(this,"", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    private static final String TAG = "MainActivity";
+
+    private BluetoothAdapter mBluetoothAdapter;
 }
